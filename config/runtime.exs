17222b7209d448349894c73c438744688c17b1fb
Nabil Lemenuel <nabil.lemenuel@gmail.com>
2026-03-16 22:29:46 +0100

    fix env variables: remove dex api key, add suspicious terms list filepath in gitignore

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,19 +4,7 @@ if config_env() == :dev do
   Dotenvy.source!([".env"]) |> System.put_env()
 end
 
-dexscreener_api_key = System.get_env("DEXSCREENER_API_KEY")
 suspicious_terms_file_path = System.get_env("SUSPICIOUS_TERMS_FILE_PATH")
-
-if is_nil(dexscreener_api_key) and config_env() != :test do
-  raise """
-  DEXSCREENER_API_KEY environment variable is required!
-
-  In development, add it to .env file:
-    DEXSCREENER_API_KEY=your_key_here
-
-  In production, set it as an environment variable.
-  """
-end
 
 if is_nil(suspicious_terms_file_path) and config_env() != :test do
   raise """
@@ -42,5 +30,4 @@ if is_binary(suspicious_terms_file_path) and config_env() != :test and
 end
 
 config :bentley,
-  dexscreener_api_key: dexscreener_api_key,
   suspicious_terms_file_path: suspicious_terms_file_path
